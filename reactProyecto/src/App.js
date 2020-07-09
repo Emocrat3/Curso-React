ca95f56bec5e243f18c6c07f5c1170086c957dec
Arthuro Ali Gomez <64677836+Emocrat3@users.noreply.github.com>
2020-07-09 16:50:05 -0400

    Primer componente y recorrido de array

--- reactProyecto/src/App.js
+++ reactProyecto/src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
+
+// Importar componentes 
+import MiComponente from './components/MiComponente';
+
+
+function holaMundo(nombre, edad){
+  var presentacion = (
+    <div>
+      <h2>Hola, soy {nombre}</h2>
+      <h3>Tengo {edad} años</h3>
+    </div>
+    );
+
+  return presentacion;
+}
 
 function App() {
+  var nombre = "Arthuro Gomez";
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenido al curso de react de Arthuro Gomez
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {holaMundo(nombre, 20)}
+        
       </header>
+      <section className="componentes">
+
+      <MiComponente />
+
+      </section>
     </div>
   );
 }
